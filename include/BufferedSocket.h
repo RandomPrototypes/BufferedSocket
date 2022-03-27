368c412c927ad1a5b2edd88d37bef78f53586bb2
RandomPrototypes <delfare1-randomprototypes@yahoo.com>
2022-03-27 14:45:28 +0900

    add dllimport

--- include/BufferedSocket.h
+++ include/BufferedSocket.h
@@ -1,12 +1,16 @@
 #pragma once
 
 #ifndef BUFSOCK_EXPORTS
-# if (defined _WIN32 || defined WINCE || defined __CYGWIN__) && defined(BUFFEREDSOCKET_EXPORTS)
-#   define BUFSOCK_EXPORTS __declspec(dllexport)
+# if (defined _WIN32 || defined WINCE || defined __CYGWIN__)
+#   if defined(BUFFEREDSOCKET_EXPORTS)
+#       define BUFSOCK_EXPORTS __declspec(dllexport)
+#   else
+#       define BUFSOCK_EXPORTS __declspec(dllimport)
+#   endif
 # elif defined __GNUC__ && __GNUC__ >= 4 && (defined(BUFFEREDSOCKET_EXPORTS) || defined(__APPLE__))
 #   define BUFSOCK_EXPORTS __attribute__ ((visibility ("default")))
 # else
-# define BUFSOCK_EXPORTS
+#   define BUFSOCK_EXPORTS
 # endif
 #endif
 
